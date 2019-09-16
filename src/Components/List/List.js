96714a96d6d08685e920c4f952e6723af32dc81b
FrunzGrigoryan <grigoryanfrunz98@gmail.com>
2019-09-16 21:06:54 +0400

    my first app

---
+++ src/Components/List/List.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './List.css'; 
+class LIst extends React.Component {
+constructor(){
+    super();
+    this.state={
+        londig:false,
+        currencies:[],
+        error:null,
+    }
+}
+}
+export default List;
